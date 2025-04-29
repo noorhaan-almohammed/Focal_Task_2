7b09be3c7406829e5dfc312d1ff5bac29dd203cd
Noorhaan Almohammed <49063826+noorhaan-almohammed@users.noreply.github.com>
2025-04-29 23:36:17 +0300

    Update FAQSection.jsx| remove -

--- src/components/FAQSection/FAQSection/FAQSection.jsx
+++ src/components/FAQSection/FAQSection/FAQSection.jsx
@@ -33,7 +33,7 @@ const faqItems = [
   {
     question: "How do you handle food allergies and dietary restrictions?",
     answer:
-      -"Our school hours are from 8:00 AM to 3:00 PM, Monday to Friday. We also offer extended care options for parents who need early drop-off or late pick-up.",
+      "Our school hours are from 8:00 AM to 3:00 PM, Monday to Friday. We also offer extended care options for parents who need early drop-off or late pick-up.",
   },
   {
     question:
